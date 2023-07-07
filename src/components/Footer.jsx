a144ec0ceabc5cf1f42034a736e7638a70caa306
Functionup-mn <nadeem2mau@gmail.com>
2023-07-08 01:30:01 +0530

    first react project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import '../styles/footer.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/mohammad-nadeem-712680213">
+            LinkedIn
+          </a>
+          <a href="https://instagram.com/mohammadnadeem1570?igshid=MzNlNGNkZWQ4Mg==">
+            Instagram
+          </a>
+          <a href="https://github.com/Functionup-mn/Lithium-Batch">GitHub</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

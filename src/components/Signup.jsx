939786b11c56617d80ad519ac3ee1e4e83ee00d0
Functionup-mn <nadeem2mau@gmail.com>
2023-07-25 23:35:59 +0530

    react-video app done

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { Avatar, Button, Container, Heading, Input, Text, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const SignUp = () => {
-    return <div>Signup</div>
-} 
+const Signup = () => {
+  return (
+    <Container maxW={'container.xl'} h={'110vh'} p={'16'}>
+      <form>
+        <VStack
+          alignItems={'stretch'}
+          spacing={'8'}
+          w={['full','96']}
+          m={'auto'}
+          my={'16'}
+        >
+          <Heading alignSelf={'center'}>VIDEO HUB</Heading>
+          <Avatar alignSelf={'center'} boxSize={'32'}/>
 
-export default SignUp
+          <Input
+            placeholder="Name"
+            type="text"
+            required
+            focusBorderColor="purple"
+          />
+
+          <Input
+            placeholder="Email"
+            type="email"
+            required
+            focusBorderColor="purple"
+          />
+
+          <Input
+            placeholder="Password"
+            type="password"
+            required
+            focusBorderColor="purple"
+          />
+
+          <Button colorScheme='purple' type='submit'>
+            Sign Up
+          </Button>
+          <Text textAlign={'right'}>
+            Already Registerd? {'  '}
+            <Button variant={'link'} colorScheme='purple'>
+                <Link to={'/login'}>Log In</Link>
+            </Button>
+            </Text>
+        </VStack>
+      </form>
+    </Container>
+  );
+};
+
+export default Signup;
